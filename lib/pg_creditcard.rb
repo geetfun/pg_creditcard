82ed3b9eb13ed30d0f9ddb419dd7a80f8511fdab
Simon Chiu <skhchiu@gmail.com>
2010-11-16 23:18:54 -0500

    updated gem name

---
+++ lib/pg_creditcard.rb
@@ -0,0 +1,53 @@
+require 'creditcard'
+
+class PgCreditcard
+  
+  def initialize(options)
+    raise ArgumentError if options.nil?
+    @name   = options[:name]
+    @number = options[:number]
+    @month  = options[:month]
+    @year   = set_year(options[:year])
+    @cvv    = options[:cvv]
+  end
+  
+  def type
+    return nil if @number.nil?
+    @type ||= @number.to_s.creditcard_type
+  end
+  
+  def valid?
+    @number.to_s.creditcard?
+  end
+  
+  def expired?
+    today = Time.now
+    expiry = Time.new(@year, @month, days_in_a_month(@year, @month))
+    today > expiry
+  end
+  
+  private
+  
+  def set_year(x)
+    if x.length == 2
+      year = "20".concat(x)
+    else
+      year = x
+    end
+    year
+  end
+  
+  def days_in_a_month(year, month)
+    if month == 12
+      year += 1
+      next_month = 1
+    else
+      next_month = month.to_i + 1
+    end
+    next_month_first_day = Time.new(year, next_month)
+    last_day_current_month = (next_month_first_day-1).day
+    days = last_day_current_month
+    days
+  end
+  
+end
--- test/test_pg_creditcard.rb
+++ test/test_pg_creditcard.rb
@@ -1,7 +1,89 @@
 require 'helper'
 
 class TestPgCreditcard < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  
+  def test_raises_argument_error_if_options_nil
+    assert_raise(ArgumentError) do
+      @cc = PgCreditcard.new
+    end
   end
+  
+  def test_does_not_raise_argument_error_if_options_nil
+    assert_nothing_raised(ArgumentError) do
+      @cc = PgCreditcard.new(
+        :name => "Homer Simpsons",
+        :number => "4111111111111111",
+        :month  => "03",
+        :year   => "20",
+        :cvv    => "123"
+      )
+    end
+  end
+
+  def test_initializing_new_pg_creditcard_instance
+    @cc = PgCreditcard.new(
+      :name => "Homer Simpsons",
+      :number => "4111111111111111",
+      :month  => "03",
+      :year   => "20",
+      :cvv    => "123"
+    )
+    assert @cc, "PgCreditcard instance not set"
+  end
+  
+  def test_determine_cc_type_from_cc_number
+    @cc = PgCreditcard.new(
+      :name => "Homer Simpsons",
+      :number => "4111111111111111",
+      :month  => "03",
+      :year   => "20",
+      :cvv    => "123"
+    )
+    assert_equal @cc.type, "visa"
+  end
+  
+  def test_return_false_if_not_expired
+    @cc = PgCreditcard.new(
+      :name => "Homer Simpsons",
+      :number => "4111111111111111",
+      :month  => "03",
+      :year   => "12",
+      :cvv    => "123"
+    )
+    assert !@cc.expired?, "PgCreditcard is expired when it is not"
+  end
+  
+  def test_return_true_if_expired
+    @cc = PgCreditcard.new(
+      :name => "Homer Simpsons",
+      :number => "4111111111111111",
+      :month  => "03",
+      :year   => "10",
+      :cvv    => "123"
+    )
+    assert @cc.expired?, "PgCreditcard is not expired when it should be expired"
+  end
+  
+  def test_return_false_if_invalid_credit_card
+    @cc = PgCreditcard.new(
+      :name => "Homer Simpsons",
+      :number => "4111111111111110",
+      :month  => "03",
+      :year   => "20",
+      :cvv    => "123"
+    )
+    assert !@cc.valid?, "PgCreditcard is valid when credit card number is invalid"
+  end
+  
+  def test_return_true_if_valid_credit_card
+    @cc = PgCreditcard.new(
+      :name => "Homer Simpsons",
+      :number => "4111111111111111",
+      :month  => "03",
+      :year   => "10",
+      :cvv    => "123"
+    )
+    assert @cc.valid?, "PgCreditcard is invalid when credit card number is valid"
+  end
+  
 end
